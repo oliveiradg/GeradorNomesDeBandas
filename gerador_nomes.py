c5ac428786b6b2af85153127b760c0cd5f53223d
Joao Oliveira <j.oliveiradg@gmail.com>
2022-07-11 16:21:04 -0300

    Gerador de nomes de bandas - py
    Projeto para o desafio de #100DaysOfPython - Dev JB Oliveira

---
+++ gerador_nomes.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Bem-Vindos ao Gerador de Nomes de Bandas.\n")
+#2. Ask the user for the city that they grew up in.
+city = input("Digite o nome de uma Cidade \n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual o nome do seu pet favorito? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da sua Banda pode ser:  " + city + " " + pet )
+
+# Projeto para o desafio de #100DaysOfPython - Dev JB Oliveira
